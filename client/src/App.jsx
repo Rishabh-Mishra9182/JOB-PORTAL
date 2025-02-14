beaf53db55fa6dcdf2d19ca312ca85792b1551de
Rishabh-Mishra9182 <rishabh.mishra@innobitsystems.com>
2025-02-14 17:18:14 +0530

    tenth commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,10 @@ import ApplyJob from "./pages/ApplyJob";
 import Applications from "./pages/Applications";
 import RecuiterLogin from "./components/RecuiterLogin";
 import { AppContext } from "./context/AppContext";
+import Dashboard from "./pages/Dashboard";
+import AddJob from "./pages/AddJob";
+import ManageJobs from "./pages/ManageJobs";
+import ViewApplication from "./pages/ViewApplication";
 
 const App = () => {
   const { showRecuiterLogin } = useContext(AppContext);
@@ -15,6 +19,11 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
         <Route path="/applications" element={<Applications />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+        <Route path="add-job" element={<AddJob />}/>
+        <Route path="manage-jobs" element={<ManageJobs />}/>
+        <Route path="view-application" element={<ViewApplication />}/>
+        </Route>
       </Routes>
     </div>
   );
